bcd171abe7a18d9a821f8306d40d5a910fcd57a0
cristian565 <crismal565@gmail.com>
2021-09-18 16:05:17 -0500

    Prueba

---
+++ apps/booking/pages/hello-booking.tsx
@@ -0,0 +1,14 @@
+import './hello-booking.module.css';
+
+/* eslint-disable-next-line */
+export interface HelloBookingProps {}
+
+export function HelloBooking(props: HelloBookingProps) {
+  return (
+    <div>
+      <h1>Welcome to HelloBooking!</h1>
+    </div>
+  );
+}
+
+export default HelloBooking;
